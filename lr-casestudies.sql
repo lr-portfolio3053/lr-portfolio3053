96b80466ab6f5c0cde4bfb975665d47033ac723d
Gar Nish <basileus336@proton.me>
2026-06-04 20:17:27 -0400

    Create lr-casestudies.sql

---
+++ lr-casestudies.sql
@@ -0,0 +1,34 @@
+/* Case studies
+
+ETL Pipeline
+
+  #1  Staging Code Aggregates - display COUNT() batches of accounts at identified active stage code
+  #2  ETL Process Automation Schedule - display automation schdule by date, by vendor
+  #3  ETL Process Automation - display ruleset pertinent to ETL processes
+  #4  ETL Process FTP Log - display ETL FTP log to verify transmission status
+  #5  ETL Process Metrics - batch statistics: account values 
+
+Controls and Monitoring
+
+  #5  Mail Vendor Report - display data pertinent to mail vendor for quick view when working accounts
+  #6  Account Close Status Report
+  #7  Address Verification Vendor Report - reports accounts processed by state for the week of the previous Monday
+  #  Placement Code Status Report 
+
+Application
+
+  #  Import file generation - prepending fields with characters required for application import
+  #  Identify Blank Automation Rules
+  #  Application Automation Scheduler Status Report
+  #  User Login Info - report login date and time by user
+  
+
+Database
+
+  #  INFORMATION_SCHEMA_COLUMNS query - display information schema of a given table
+  #  Distinct items in table
+  #  Duplicate CTE
+  #  Maximum field length of a given field
+  
+  
+*/
